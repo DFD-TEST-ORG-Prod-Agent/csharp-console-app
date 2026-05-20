b7d9b6531ba78aaacf34cc9926c713b96524ab39
Michael Sterenberg <michast@microsoft.com>
2026-05-20 09:32:15 +0300

    Add sample application code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+            
+            // Simple calculation
+            int result = Calculate(10, 5);
+            Console.WriteLine($"10 + 5 = {result}");
+        }
+        
+        static int Calculate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
